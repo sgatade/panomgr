1b860d96f0cdd3d0c184d08a1896e77a684c5160
sg <sgatade@gmail.com>
2019-12-04 22:54:07 +0100

    Added async await to logger, basic user create test completed

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,23 +3,23 @@ const chalk = require("chalk");
 const y = "[panomgr] ";
 
 // d : Debug
-const d = (mesg) => {
-    console.log(chalk.blue(y + mesg));
+const d = async (mesg) => {
+    await console.log(chalk.blue(y + mesg));
 };
 
 // i : info
-const i = (mesg) => {
-    console.log(chalk.yellow(y + mesg));
+const i = async (mesg) => {
+    await console.log(chalk.yellow(y + mesg));
 };
 
 // s : success
-const s = (mesg) => {
-    console.log(chalk.green(y + mesg));
+const s = async (mesg) => {
+    await console.log(chalk.green(y + mesg));
 }
 
 // e : error
-const e = (mesg) => {
-    console.log(chalk.red(y + mesg));
+const e = async (mesg) => {
+    await console.log(chalk.red(y + mesg));
 }
 
 module.exports = {d, i, s, e};
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,12 @@
+const request = require("supertest");
+const express = require("express");
+const App = require("../src/app");
 const User = require("../src/models/users");
 
-test('Test User', () => {
-    console.log('testing');
-});
-
-test("Async test", (done) => {
-
-    setTimeout(() => {
-        expect(1).toBe(1);    
-        done();
-    }, 2000);
+test('Should create a new user', async () => {
+    const user = 
+    await request(App).post("/api/users").send({
+        name: "Test User",
+        password: "Pass1982*"
+    }).expect(200);
 });
